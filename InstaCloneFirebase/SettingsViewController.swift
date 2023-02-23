0f916463a8038b891f08378f723ba0e4791c0f6b
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-02-23 09:21:51 +0300

    Ayarlar VC

---
+++ InstaCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Kerem Tuna Tomak on 23.02.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
